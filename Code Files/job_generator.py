c77cc675ec25daa94808c217300abbcd9f9c1517
Abhinav <abhinavsunil@hotmail.com>
2025-03-27 22:01:51 +0530

    added track track in generating jobs

--- Code Files/job_generator.py
+++ Code Files/job_generator.py
@@ -17,6 +17,6 @@ def generate_jobs(num_jobs, arrival_range, priority_range, track_range):
     for pid in range(num_jobs):
         arrival_time = random.randint(arrival_range[0], arrival_range[1])
         priority = random.randint(priority_range[0], priority_range[1])
-        track = random.randint(track_range[0], track_range[1])
+        track = random.randint(track_range[0], track_range[1]) ## added track_range
         jobs.append(Job(pid, arrival_time, priority, track))
     return jobs
